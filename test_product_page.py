9267cb9db496bb6966f5295eef5827a02f9f67c5
Konstantinjj <kohctahtuh.pog@yandex.ru>
2024-03-30 02:59:26 +0300

    Bug hunting from a list of URL addresses

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,15 @@
 from .pages.product_page import ProductPage
+import pytest
+
+links = [
+    f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}" for i in range(10)
+]
+# Помечаю падающие страницы
+links[7] = pytest.param(links[7], marks=pytest.mark.xfail)
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize("link", links)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.test_add_to_basket()
