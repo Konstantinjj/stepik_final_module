639fb40dbc40ae914ad71347741aa2a89ab217e1
Konstantinjj <kohctahtuh.pog@yandex.ru>
2024-03-29 15:31:57 +0300

    Implementation of tests for adding to basket from product page https://stepik.org/lesson/201964/step/2?unit=176022

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, ".alert-success strong")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
---
+++ pages/product_page.py
@@ -0,0 +1,34 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def test_add_to_basket(self):
+        self.should_be_add_to_basket_button()
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button.click()
+        BasePage.solve_quiz_and_get_code(self)
+        self.should_be_product_added_to_basket()
+        self.should_be_correct_basket_price()
+
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BUTTON), \
+            "Add to basket button is not presented"
+
+    def get_product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def get_product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def should_be_product_added_to_basket(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_IN_BASKET), \
+            "Product in basket is not presented"
+        product_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET)
+        assert self.get_product_name() == product_in_basket.text, "Product name in message is not correct"
+
+    def should_be_correct_basket_price(self):
+        assert self.is_element_present(*ProductPageLocators.BASKET_PRICE), \
+            "Basket price is not presented"
+        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE)
+        assert self.get_product_price() == basket_price.text, "Product price is not equal basket price"
